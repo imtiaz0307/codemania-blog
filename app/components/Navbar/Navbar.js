00e072a67f3c637c941c8110e11960b99ac289b3
imtiaz0307 <imtiaz0307@gmail.com>
2023-02-09 18:18:59 +0500

    Made the blog responsive.

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -2,15 +2,25 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import styles from './Navbar.module.css'
 import { FiSearch } from 'react-icons/fi'
 import { HiXMark } from 'react-icons/hi2'
+import { useRouter } from 'next/navigation'
 
 
 
 const Navbar = () => {
+    const router = useRouter()
     const [showSearchForm, setShowSearchForm] = useState(false)
+    const [searchQuery, setSearchQuery] = useState('')
+
+    // searchHandler
+    const searchHandler = (e) => {
+        e.preventDefault()
+        router.push('/blogs')
+        setShowSearchForm(false)
+    }
 
     return (
         <header className={styles.header}>
@@ -20,9 +30,9 @@ const Navbar = () => {
             </Link>
             <nav className={styles.nav}>
                 <ul className={styles.mainMenu}>
-                    <li>
+                    {/* <li>
                         <FiSearch fontSize={'.9rem'} style={{ transform: 'translateY(3px)', cursor: 'pointer' }} onClick={() => setShowSearchForm(true)} />
-                    </li>
+                    </li> */}
                     <li>
                         <Link href={'/'}>HOME</Link>
                     </li>
@@ -37,9 +47,9 @@ const Navbar = () => {
                     </li>
                 </ul >
             </nav >
-            <form className={styles.searchForm} style={{ display: !showSearchForm ? 'none' : 'flex' }}>
+            <form className={styles.searchForm} style={{ display: !showSearchForm ? 'none' : 'flex' }} onSubmit={searchHandler}>
                 <HiXMark fontSize={'2rem'} style={{ position: 'absolute', top: '15px', right: '15px', cursor: 'pointer' }} onClick={() => setShowSearchForm(false)} />
-                <input type="search" id="searchInput" placeholder='Search blog posts...' />
+                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="search" id="searchInput" placeholder='Search blog posts...' />
                 <button type="submit">
                     <FiSearch fontSize={'1.6rem'} style={{ transform: 'translateY(3px)', cursor: 'pointer' }} onClick={() => setShowSearchForm(true)} />
                 </button>
