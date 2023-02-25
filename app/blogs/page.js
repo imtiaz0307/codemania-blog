fa92e10d70c5d58f34977c6d3f4df388f63b7c2d
imtiaz0307 <imtiaz0307@gmail.com>
2023-02-26 00:25:00 +0500

    implemented the search functionality.

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -5,9 +5,25 @@ import styles from './Blogs.module.css'
 import { blogPosts } from "@/data/blogPosts";
 import Filter from "../components/Filter/Filter";
 import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+
 
 const BlogsPage = () => {
+    const search = useSearchParams()
+    const searchQuery = search.get('search')
+
     const [blogs, setBlogs] = useState(blogPosts)
+
+    const filtered = blogs.filter(blog => {
+        return blog.title.toLowerCase().includes(searchQuery)
+            ||
+            blog.description.toLowerCase().includes(searchQuery)
+            ||
+            blog.slug.toLowerCase().includes(searchQuery)
+            ||
+            blog.metadesc.toLowerCase().includes(searchQuery)
+    })
+
     return (
         <div className={styles.blogsPage}>
 
@@ -15,13 +31,19 @@ const BlogsPage = () => {
 
             <div className={styles.blogs}>
                 {
-                    blogs.length > 0
+                    searchQuery
                         ?
-                        blogs.map((blog, index) => {
+                        filtered.map((blog, index) => {
                             return <BlogCard key={index} blog={blog} />
                         })
                         :
-                        'No Blogs To Show'
+                        blogs.length > 0
+                            ?
+                            blogs.map((blog, index) => {
+                                return <BlogCard key={index} blog={blog} />
+                            })
+                            :
+                            'No Blogs To Show'
                 }
             </div>
         </div>
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -2,24 +2,24 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import styles from './Navbar.module.css'
 import { FiSearch } from 'react-icons/fi'
 import { HiXMark } from 'react-icons/hi2'
-import { useRouter } from 'next/navigation'
 
 
 
 const Navbar = () => {
-    const router = useRouter()
+    // const router = useRouter()
     const [showSearchForm, setShowSearchForm] = useState(false)
     const [searchQuery, setSearchQuery] = useState('')
+    const searchQueryRef = useRef()
 
-    // searchHandler
     const searchHandler = (e) => {
         e.preventDefault()
-        router.push('/blogs')
+        searchQueryRef.current.click()
         setShowSearchForm(false)
+        setSearchQuery('')
     }
 
     return (
@@ -30,9 +30,9 @@ const Navbar = () => {
             </Link>
             <nav className={styles.nav}>
                 <ul className={styles.mainMenu}>
-                    {/* <li>
+                    <li>
                         <FiSearch fontSize={'.9rem'} style={{ transform: 'translateY(3px)', cursor: 'pointer' }} onClick={() => setShowSearchForm(true)} />
-                    </li> */}
+                    </li>
                     <li>
                         <Link href={'/'}>HOME</Link>
                     </li>
@@ -50,9 +50,11 @@ const Navbar = () => {
             <form className={styles.searchForm} style={{ display: !showSearchForm ? 'none' : 'flex' }} onSubmit={searchHandler}>
                 <HiXMark fontSize={'2rem'} style={{ position: 'absolute', top: '15px', right: '15px', cursor: 'pointer' }} onClick={() => setShowSearchForm(false)} />
                 <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="search" id="searchInput" placeholder='Search blog posts...' />
-                <button type="submit">
+
+                <button type='submit'>
                     <FiSearch fontSize={'1.6rem'} style={{ transform: 'translateY(3px)', cursor: 'pointer' }} onClick={() => setShowSearchForm(true)} />
                 </button>
+                <Link href={{ pathname: '/blogs', query: { search: searchQuery } }} ref={searchQueryRef} />
             </form>
         </header >
     )
