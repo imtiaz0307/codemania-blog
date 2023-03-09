a03a1a869ff71cd4f89a138bd9359267932449b2
imtiaz0307 <imtiaz0307@gmail.com>
2023-03-09 10:43:24 +0500

    fixed the reloading issue while switching routes.

--- app/blogs/[slug]/loading.js
+++
@@ -1,7 +0,0 @@
-import { Spinner } from "../../components/Spinner/Spinner"
-
-const Loading = () => {
-    return <Spinner />
-}
-
-export default Loading
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+import { Suspense } from 'react'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import './globals.css'
+import Loading from './loading'
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +10,9 @@ export default function RootLayout({ children }) {
       <head />
       <body style={{ maxWidth: '1300px', margin: 'auto' }}>
         <Navbar />
-        {children}
+        <Suspense fallback={<Loading />}>
+          {children}
+        </Suspense>
         <Footer />
       </body>
     </html>
